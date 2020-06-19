dc3b787bb09a4cc1fb5742970af1d28e6ca93057
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-06-19 21:35:17 +0200

    small fix in styles, change svg and classes, querySelectors in JS

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,7 @@ registerSW();
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const addGlass = document.querySelector('.add--js')
+const removeGlass = document.querySelector('.remove--js')
+const amount = document.querySelector('.amount--js')
