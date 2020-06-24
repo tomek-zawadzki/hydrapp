dd963f09f5f903af296f1d307937fe6f7d0703d0
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-06-25 00:12:54 +0200

    add code to index.js, functions for buttons, and localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,44 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
 
-const addGlass = document.querySelector('.add--js')
-const removeGlass = document.querySelector('.remove--js')
-const amount = document.querySelector('.amount--js')
+const addGlass = document.querySelector(".add--js");
+const removeGlass = document.querySelector(".remove--js");
+const amount = document.querySelector(".amount--js");
+const key = new Date().toLocaleString().slice(0, 10);
+console.log(key);
+
+let counter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+  counter = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
+}
+
+amount.innerHTML = counter;
+
+addGlass.addEventListener("click", () => {
+  counter++;
+  amount.innerHTML = counter;
+  localStorage.setItem(key, counter);
+});
+
+removeGlass.addEventListener("click", () => {
+  if (counter > 0) {
+    counter--;
+  }
+  amount.innerHTML = counter;
+  localStorage.setItem(key, counter);
+});
+
+
+// parseInt() - zamiana stringa na number
